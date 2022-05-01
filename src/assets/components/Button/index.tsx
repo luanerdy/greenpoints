e95d7a182074d2b000c46eed5e0ce0e827ac899b
luanerdy <luan.vini1@gmail.com>
2022-05-01 14:16:52 -0300

    fix: build

--- src/assets/components/Button/index.tsx
+++ src/assets/components/Button/index.tsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 import { Button_style } from "./styles";
 
-export const Button_Green = ({andress,text}) =>{
+export const Button_Green = (props: {andress: string, text: string}) =>{
+    const {andress, text} = props;
+
     return(
         <Button_style>
             <Link to={andress}>{text}</Link>
         </Button_style>
-        
+
     )
 }
