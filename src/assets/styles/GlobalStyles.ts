9a7665e9b03d733cef05dc3cd42ee2211f30daaa
PATRICKOTERONOT\patri <patrick.rocha.otero@gmail.com>
2022-05-01 14:41:43 -0300

    header que será usado em todas as páginas autenticadas está pronto e renderizado no componente Home

--- src/assets/styles/GlobalStyles.ts
+++ src/assets/styles/GlobalStyles.ts
@@ -1,5 +1,5 @@
-import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
+import { createGlobalStyle } from 'styled-components'
+import reset from 'styled-reset'
 
 export const GlobalStyles = createGlobalStyle`
   ${reset}
@@ -8,4 +8,16 @@ export const GlobalStyles = createGlobalStyle`
     --color-Dark-Button: #025940;
     --color-Light-Button: #03A64A;
   }
-`;
+  
+  * {
+  padding: 0;
+  margin: 0;
+  
+  box-sizing: border-box
+  }
+
+  html {
+    font-size: 62.5%
+    }
+    
+`
